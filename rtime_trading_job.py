c8aee0b07bb30c8a45765c41969158c7ac674a44
yanbixing <yanbixing@jiigan.com>
2025-06-24 20:04:04 +0800

    [250624] 将开盘啦板块排名加到每日任务中。

--- rtime_trading_job.py
+++ rtime_trading_job.py
@@ -2,6 +2,7 @@ import rtime_subjob.hongpanjiashu as hpjs
 import rtime_subjob.jingjiashujukuaibao as jjsjkb
 import rtime_subjob.shichanggailan as scgl
 import rtime_subjob.xingutixing as xgtx
+import rtime_subjob.kaipanla_sector as kplsec
 from concurrent.futures import ThreadPoolExecutor
 
 
@@ -11,7 +12,7 @@ def main():
         executor.submit(hpjs.hongpanjiashu_rtime_jobs)
         executor.submit(scgl.shichanggailan_rtime_jobs)
         executor.submit(xgtx.xingutixing_rtime_jobs)
-
+        executor.submit(kplsec.kaipanla_sector_rtime_jobs)
 #实时任务，由各个小模块自己控制发送时间，这里只作为一个启动入口
 if __name__ == "__main__":
     main()
